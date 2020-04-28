498afe84367f1a171897de6243c903508dbe6d8d
Yushan Cheng <b10432020@gapps.ntust.edu.tw>
2020-04-28 16:12:44 +0800

    render search result

--- index.js
+++ index.js
@@ -2,38 +2,29 @@
 
 let proxyServer = `https://still-spire-37210.herokuapp.com/positions.json?`
 let burger = document.querySelector("#navbar-burger");
-// let search = 
 
 burger.addEventListener("click", function(e){
-	document.querySelector(".burger").classList.toggle("is-active")
-	document.querySelector(".navbar-menu").classList.toggle("is-active")
+	document.querySelector(".burger").classList.toggle("is-active");
+	document.querySelector(".navbar-menu").classList.toggle("is-active");
 })
-
-// fetch('http://example.com/movies.json')
-//   .then((response) => {
-//     return response.json();
-//   })
-//   .then((data) => {
-//     console.log(data);
-//   });
 
 document.addEventListener("submit", function(e) {
 	e.preventDefault();
 	let description = document.forms[0]["description"].value;
 	let location = document.forms[0]["location"].value;
-	let full_time_checked = document.forms[0]["full_time"].checked;
+	let full_time_checked = trueToOn(document.forms[0]["full_time"].checked);
 
 	let searchParams = new URLSearchParams([['description', description], ['location', location], ['full_time', full_time_checked]]);
-	// console.log(`${searchParams}`);
-
-	console.log(getUrl(searchParams));
 
 	fetch(getUrl(searchParams))
 	  .then((response) => {
 	    return response.json();
 	  })
 	  .then((data) => {
-	    console.log(data);
+	  	let jobPannel = document.querySelector("#job-pannel");
+	  	let result = data.map(toHTML).join("");
+	    jobPannel.innerHTML = result;
+	    
 	  });
 })
 
@@ -41,6 +32,36 @@ document.addEventListener("submit", function(e) {
 function getUrl(searchParams){
 	return proxyServer +`${searchParams}`
 }
+
+
+function toHTML(data){
+	return `
+		<tr>
+		            <td>
+		              <h4><a href="${data.url}">${data.title}</a></h4>
+		              <p class="source">
+		              <a class="company" href="${data.company_url}">${data.company}</a>
+		              –
+		              <strong class="fulltime">${data.type}</strong>
+		              </p>
+		            </td>
+		            <td class="meta">
+		              <span class="location">${data.location}</span>
+		            </td>
+	          </tr>
+    `;
+}
+
+function trueToOn(params){
+	if (params === true)
+	{
+		return "on";
+	} else {
+		return ""
+	}
+}
+
+
 
 
 
